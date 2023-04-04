99d0ead552751e4028a62edb74594d3945353719
Aydan221 <129885253+Aydan221@users.noreply.github.com>
2023-04-04 10:09:08 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+ public static void main(String[]args){
+    Scanner input = new Scanner  (System.in);
+    System.out.print("Enter a name:");
+    String name = input.next();
+    System.out.print("Enter activity:");
+    String activity = input.next();
+    System.out.println( name + " likes to " + activity + ".");
+  }
+ }//created by aydan
